a8101f4040e3b57afdf38efa6f880aa7d8e0408b
kholdfuzion <kholdfuzion@gmail.com>
2025-01-30 12:05:34 -0500

    Update build.sh
    seems to be removed now upstream

--- build.sh
+++ build.sh
@@ -14,4 +14,4 @@ pacman --noconfirm -Syu btrfs-progs archiso git reflector
 git clone https://github.com/holoiso-staging/buildroot buildroot 
 git clone https://github.com/holoiso-staging/postcopy -b ${POSTCOPY_CLONE} buildroot/postcopy_$1 
 mkdir -p $(pwd)/output $(pwd)/workdir 
-bash buildroot/build.sh --flavor $1 --deployment_rel $1 --snapshot_ver "${GITHUB_REF_NAME}" --workdir $(pwd)/workdir --output-dir $(pwd)/output/holoiso-images --add-release
+bash buildroot/build.sh --flavor $1 --snapshot_ver "${GITHUB_REF_NAME}" --workdir $(pwd)/workdir --output-dir $(pwd)/output/holoiso-images --add-release
